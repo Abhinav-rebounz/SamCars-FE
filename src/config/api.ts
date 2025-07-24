94607fe11311d8712c2fe081209eb7ffea7574e8
vamsi-rebounz <vamsi.cheruku@rebounz.org>
2025-07-23 20:46:05 -0500

    Prod issues fixed

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://localhost:3000/api';
+// export const API_BASE_URL = 'http://localhost:3000/api';
+export const API_BASE_URL = 'https://api.saamcars.com/api';
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
